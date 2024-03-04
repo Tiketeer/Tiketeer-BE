8a5ee6899b3b033079465605788b27fd55473af2
One-armed-boy <fanatic5500@gmail.com>
2024-03-04 15:34:47 +0900

    init project

---
+++ src/main/java/com/tiketeer/Tiketeer/TiketeerApplication.java
@@ -0,0 +1,13 @@
+package com.tiketeer.Tiketeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiketeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiketeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiketeer/Tiketeer/TiketeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiketeer.Tiketeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiketeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
