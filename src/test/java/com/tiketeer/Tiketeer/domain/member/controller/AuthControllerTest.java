10a91b977831c15855d4c886f1df981b44245590
찰흙고양이 <claaycaat@gmail.com>
2024-03-18 20:16:26 +0900

    implement fail case for login test

---
+++ src/test/java/com/tiketeer/Tiketeer/domain/member/controller/AuthControllerTest.java
@@ -0,0 +1,10 @@
+package com.tiketeer.Tiketeer.domain.member.controller;
+
+import org.junit.jupiter.api.Test;
+
+class AuthControllerTest {
+
+	@Test
+	void login() {
+	}
+}
--- src/test/java/com/tiketeer/Tiketeer/domain/member/service/LoginServiceTest.java
+++ src/test/java/com/tiketeer/Tiketeer/domain/member/service/LoginServiceTest.java
@@ -3,6 +3,7 @@ package com.tiketeer.Tiketeer.domain.member.service;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import com.tiketeer.Tiketeer.auth.jwt.JwtPayload;
 import com.tiketeer.Tiketeer.auth.jwt.JwtService;
 import com.tiketeer.Tiketeer.domain.member.Member;
+import com.tiketeer.Tiketeer.domain.member.exception.InvalidLoginException;
 import com.tiketeer.Tiketeer.domain.member.repository.MemberRepository;
 import com.tiketeer.Tiketeer.domain.member.service.dto.GenerateCookieCommand;
 import com.tiketeer.Tiketeer.domain.role.Role;
@@ -30,8 +32,6 @@ import jakarta.servlet.http.Cookie;
 class LoginServiceTest {
 	@Autowired
 	private TestHelper testHelper;
-	@Autowired
-	private MemberService memberService;
 	@Autowired
 	private RoleRepository roleRepository;
 	@Autowired
@@ -53,13 +53,8 @@ class LoginServiceTest {
 		testHelper.cleanDB();
 	}
 
-	@Test
-	@DisplayName("dd")
-	void loginSuccess() {
-
-		//given
+	private void saveMember() {
 		Role role = roleRepository.findByName(RoleEnum.BUYER).orElseThrow();
-
 		Member member = Member.builder()
 			.email("user@example.com")
 			.password(passwordEncoder.encode("password"))
@@ -67,8 +62,14 @@ class LoginServiceTest {
 			.enabled(true)
 			.role(role)
 			.build();
-
 		memberRepository.save(member);
+	}
+
+	@Test
+	@DisplayName("로그인 요청 > 로그인 > 성공")
+	void loginSuccess() {
+
+		saveMember();
 
 		GenerateCookieCommand command = GenerateCookieCommand.builder()
 			.email("user@example.com")
@@ -83,6 +84,41 @@ class LoginServiceTest {
 		JwtPayload jwtPayload = jwtService.verifyToken(token);
 		assertThat(jwtPayload.email()).isEqualTo("user@example.com");
 		assertThat(jwtPayload.roleEnum()).isEqualTo(RoleEnum.BUYER);
+	}
+
+	@Test
+	@DisplayName("존재하지 않는 이메일로 로그인 요청 > 실패")
+	void loginFailInvalidEmail() {
+		//given
+		saveMember();
+
+		GenerateCookieCommand command = GenerateCookieCommand.builder()
+			.email("nobody@example.com")
+			.password("password")
+			.build();
+
+		//when - then
+		Assertions.assertThrows(InvalidLoginException.class, () -> {
+			loginService.generateCookie(command);
+		});
+	}
+
+	@Test
+	@DisplayName("잘못된 비밀번호로 로그인 요청 > 실패")
+	void loginFailInvalidPassword() {
+		//given
+		saveMember();
+
+		GenerateCookieCommand command = GenerateCookieCommand.builder()
+			.email("user@example.com")
+			.password("invalid")
+			.build();
+
+		//when - then
+		Assertions.assertThrows(InvalidLoginException.class, () -> {
+			loginService.generateCookie(command);
+		});
 
 	}
+
 }
