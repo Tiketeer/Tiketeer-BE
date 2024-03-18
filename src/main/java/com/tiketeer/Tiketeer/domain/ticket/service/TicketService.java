1046f67573ed02c852d40875f9aa7fc10ac19557
One-armed-boy <fanatic5500@gmail.com>
2024-03-18 13:36:49 +0900

    성능 개선을 위해 TicketService.dropNumOfTicketsUnderSomeTicketing 메서드 내에서 deleteAllByIdInBatch 메서드를 사용하도록 수정

--- src/main/java/com/tiketeer/Tiketeer/domain/ticket/service/TicketService.java
+++ src/main/java/com/tiketeer/Tiketeer/domain/ticket/service/TicketService.java
@@ -66,7 +66,7 @@ public class TicketService {
 			.limit(command.getNumOfTickets())
 			.map(Ticket::getId).toList();
 
-		ticketRepository.deleteAllById(ticketIdsForDelete);
+		ticketRepository.deleteAllByIdInBatch(ticketIdsForDelete);
 	}
 
 	@Transactional
