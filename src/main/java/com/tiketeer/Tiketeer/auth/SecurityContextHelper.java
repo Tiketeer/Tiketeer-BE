ec2fe792a8ec1e236058841dfc5d02710a218c6a
One-armed-boy <fanatic5500@gmail.com>
2024-03-19 16:40:47 +0900

    SecurityContext에서 멤버 이메일을 가져오는 작업을 돕는 헬퍼 클래스 작성

---
+++ src/main/java/com/tiketeer/Tiketeer/auth/SecurityContextHelper.java
@@ -0,0 +1,11 @@
+package com.tiketeer.Tiketeer.auth;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityContextHelper {
+	public String getEmail() {
+		return SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+	}
+}
