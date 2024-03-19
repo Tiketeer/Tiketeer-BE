8b16e669246ef455b1ace75da8a9736a50d77347
찰흙고양이 <claaycaat@gmail.com>
2024-03-20 00:10:53 +0900

    Implement service and controller

--- src/main/java/com/tiketeer/Tiketeer/domain/member/controller/MemberController.java
+++ src/main/java/com/tiketeer/Tiketeer/domain/member/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.tiketeer.Tiketeer.domain.member.controller;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tiketeer.Tiketeer.domain.member.controller.dto.ChargePointRequestDto;
 import com.tiketeer.Tiketeer.domain.member.controller.dto.ChargePointResponseDto;
+import com.tiketeer.Tiketeer.domain.member.controller.dto.GetMemberTicketingSalesResponseDto;
 import com.tiketeer.Tiketeer.domain.member.controller.dto.MemberRegisterRequestDto;
 import com.tiketeer.Tiketeer.domain.member.controller.dto.MemberRegisterResponseDto;
 import com.tiketeer.Tiketeer.domain.member.service.MemberPointService;
 import com.tiketeer.Tiketeer.domain.member.service.MemberRegisterService;
+import com.tiketeer.Tiketeer.domain.member.service.MemberTicketingService;
+import com.tiketeer.Tiketeer.domain.member.service.dto.GetMemberTicketingSalesCommandDto;
 import com.tiketeer.Tiketeer.domain.member.service.dto.MemberRegisterCommandDto;
 import com.tiketeer.Tiketeer.response.ApiResponse;
 
@@ -24,12 +33,15 @@ import jakarta.validation.Valid;
 @RequestMapping("/members")
 public class MemberController {
 	private final MemberRegisterService memberRegisterService;
+	private final MemberTicketingService memberTicketingService;
 	private final MemberPointService memberPointService;
 
 	@Autowired
-	public MemberController(MemberRegisterService memberRegisterService, MemberPointService memberPointService) {
+	public MemberController(MemberRegisterService memberRegisterService, MemberPointService memberPointService,
+		MemberTicketingService memberTicketingService) {
 		this.memberRegisterService = memberRegisterService;
 		this.memberPointService = memberPointService;
+		this.memberTicketingService = memberTicketingService;
 	}
 
 	@PostMapping("/register")
@@ -53,4 +65,16 @@ public class MemberController {
 		var result = ChargePointResponseDto.builder().totalPoint(totalPoint).build();
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.wrap(result));
 	}
+
+	@GetMapping("/members/{memberId}/sale")
+	public ResponseEntity<ApiResponse<List<GetMemberTicketingSalesResponseDto>>> getMemberTicketingSales(
+		@PathVariable UUID memberId) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String email = (String)authentication.getPrincipal();
+		var result = memberTicketingService.getMemberTicketingSales(
+			new GetMemberTicketingSalesCommandDto(memberId, email));
+		var response = result.stream().map(GetMemberTicketingSalesResponseDto::convertFromResult).toList();
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.wrap(response));
+	}
+
 }
