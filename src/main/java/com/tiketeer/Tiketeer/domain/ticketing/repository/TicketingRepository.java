c04a8b85fec7b491330fabcae17214a95b7c6b4b
찰흙고양이 <claaycaat@gmail.com>
2024-03-20 00:10:27 +0900

    Write JPQL query for method

--- src/main/java/com/tiketeer/Tiketeer/domain/ticketing/repository/TicketingRepository.java
+++ src/main/java/com/tiketeer/Tiketeer/domain/ticketing/repository/TicketingRepository.java
@@ -1,12 +1,30 @@
 package com.tiketeer.Tiketeer.domain.ticketing.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.tiketeer.Tiketeer.domain.member.service.dto.GetMemberTicketingSalesResultDto;
 import com.tiketeer.Tiketeer.domain.ticketing.Ticketing;
 
 @Repository
 public interface TicketingRepository extends JpaRepository<Ticketing, UUID> {
+
+	@Query(value = """
+		select new com.tiketeer.Tiketeer.domain.member.service.dto.GetMemberTicketingSalesResultDto(
+			t.id, t.title, t.description, t.location, t.eventTime, t.saleStart, t.saleEnd,
+			(select count(tk) from Ticket tk where tk.ticketing = t),
+			(select count(tk) from Ticket tk where tk.ticketing = t and tk.purchase is null ),
+			t.createdAt, t.category, t.runningMinutes, t.price
+		)
+		from Ticketing t
+		where t.member.email = :email
+		"""
+	)
+	public List<GetMemberTicketingSalesResultDto> findTicketingWithTicketStock(@Param("email") String email);
+
 }
