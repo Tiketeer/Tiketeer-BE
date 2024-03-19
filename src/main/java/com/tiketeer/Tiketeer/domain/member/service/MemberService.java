9d2d2e6ce0e7ad190ee350af92c8be67c8cb407f
One-armed-boy <fanatic5500@gmail.com>
2024-03-19 21:09:53 +0900

    MemberService.sendPwdChangeEmail 메서드 내 비즈니스 로직에 대해 펜딩 되기 직전 작업까지 임시 커밋

--- src/main/java/com/tiketeer/Tiketeer/domain/member/service/MemberService.java
+++ src/main/java/com/tiketeer/Tiketeer/domain/member/service/MemberService.java
@@ -9,12 +9,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.tiketeer.Tiketeer.auth.jwt.JwtPayload;
 import com.tiketeer.Tiketeer.auth.jwt.JwtService;
+import com.tiketeer.Tiketeer.domain.member.Otp;
 import com.tiketeer.Tiketeer.domain.member.exception.InvalidOtpException;
 import com.tiketeer.Tiketeer.domain.member.exception.InvalidTokenException;
+import com.tiketeer.Tiketeer.domain.member.exception.MemberIdAndAuthNotMatchedException;
+import com.tiketeer.Tiketeer.domain.member.exception.MemberNotFoundException;
+import com.tiketeer.Tiketeer.domain.member.repository.MemberRepository;
 import com.tiketeer.Tiketeer.domain.member.repository.OtpRepository;
 import com.tiketeer.Tiketeer.domain.member.service.dto.InitMemberPasswordWithOtpCommandDto;
 import com.tiketeer.Tiketeer.domain.member.service.dto.RefreshAccessTokenCommandDto;
 import com.tiketeer.Tiketeer.domain.member.service.dto.RefreshAccessTokenResultDto;
+import com.tiketeer.Tiketeer.domain.member.service.dto.SendPwdChangeEmailCommandDto;
+import com.tiketeer.Tiketeer.infra.alarm.email.EmailService;
 
 import io.jsonwebtoken.JwtException;
 
@@ -23,14 +29,18 @@ import io.jsonwebtoken.JwtException;
 public class MemberService {
 	private final PasswordEncoder passwordEncoder;
 	private final OtpRepository otpRepository;
-
+	private final MemberRepository memberRepository;
 	private final JwtService jwtService;
+	private final EmailService emailService;
 
 	@Autowired
-	public MemberService(PasswordEncoder passwordEncoder, OtpRepository otpRepository, JwtService jwtService) {
+	public MemberService(PasswordEncoder passwordEncoder, OtpRepository otpRepository,
+		MemberRepository memberRepository, JwtService jwtService, EmailService emailService) {
 		this.passwordEncoder = passwordEncoder;
 		this.otpRepository = otpRepository;
+		this.memberRepository = memberRepository;
 		this.jwtService = jwtService;
+		this.emailService = emailService;
 	}
 
 	@Transactional
@@ -41,6 +51,25 @@ public class MemberService {
 		member.setPassword(passwordEncoder.encode(command.getPassword()));
 		member.setEnabled(true);
 		otpRepository.delete(otp);
+	}
+
+	@Transactional
+	public void sendPwdChangeEmail(SendPwdChangeEmailCommandDto command) {
+		var memberId = command.getMemberId();
+		var email = command.getEmail();
+
+		var member = memberRepository.findByEmail(email).orElseThrow(MemberNotFoundException::new);
+
+		if (!member.getId().equals(memberId)) {
+			throw new MemberIdAndAuthNotMatchedException();
+		}
+
+		otpRepository.findByMember(member).ifPresent(otpRepository::delete);
+
+		var otp = Otp.builder().member(member).build();
+		otpRepository.save(otp);
+		// TODO: 훈이 작성 중이신 PR 병합된 이후 작업 재개
+		// emailService.sendEmail();
 	}
 
 	public RefreshAccessTokenResultDto refreshAccessToken(RefreshAccessTokenCommandDto refreshAccessTokenCommandDto) {
