bed89118dd8b71200e9d7a07e84ba40beebff190
dla0510 <dla0510@naver.com>
2024-03-19 09:29:03 +0900

    티켓 환불 EP 작성

--- src/main/java/com/tiketeer/Tiketeer/domain/purchase/service/PurchaseService.java
+++ src/main/java/com/tiketeer/Tiketeer/domain/purchase/service/PurchaseService.java
@@ -1,7 +1,9 @@
 package com.tiketeer.Tiketeer.domain.purchase.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Limit;
@@ -11,12 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 import com.tiketeer.Tiketeer.domain.member.exception.MemberNotFoundException;
 import com.tiketeer.Tiketeer.domain.member.repository.MemberRepository;
 import com.tiketeer.Tiketeer.domain.purchase.Purchase;
+import com.tiketeer.Tiketeer.domain.purchase.exception.AccessForNotOwnedPurchaseException;
+import com.tiketeer.Tiketeer.domain.purchase.exception.EmptyPurchaseException;
 import com.tiketeer.Tiketeer.domain.purchase.exception.NotEnoughTicketException;
+import com.tiketeer.Tiketeer.domain.purchase.exception.PurchaseNotFoundException;
 import com.tiketeer.Tiketeer.domain.purchase.exception.PurchaseNotInSalePeriodException;
 import com.tiketeer.Tiketeer.domain.purchase.repository.PurchaseRepository;
 import com.tiketeer.Tiketeer.domain.purchase.service.dto.CreatePurchaseCommandDto;
 import com.tiketeer.Tiketeer.domain.purchase.service.dto.CreatePurchaseResultDto;
+import com.tiketeer.Tiketeer.domain.purchase.service.dto.DeletePurchaseTicketsCommandDto;
+import com.tiketeer.Tiketeer.domain.ticket.Ticket;
 import com.tiketeer.Tiketeer.domain.ticket.repository.TicketRepository;
+import com.tiketeer.Tiketeer.domain.ticketing.Ticketing;
 import com.tiketeer.Tiketeer.domain.ticketing.exception.TicketingNotFoundException;
 import com.tiketeer.Tiketeer.domain.ticketing.repository.TicketingRepository;
 
@@ -44,7 +52,7 @@ public class PurchaseService {
 		var member = this.memberRepository.findByEmail(createPurchaseCommandDto.getMemberEmail()).orElseThrow(
 			MemberNotFoundException::new);
 
-		canPurchaseTicketing(ticketingId, createPurchaseCommandDto.getCommandCreatedAt());
+		validateTicketingSalePeriod(ticketingId, createPurchaseCommandDto.getCommandCreatedAt());
 
 		var newPurchase = this.purchaseRepository.save(Purchase.builder().member(member).build());
 
@@ -65,7 +73,31 @@ public class PurchaseService {
 			.build();
 	}
 
-	private void canPurchaseTicketing(UUID ticketingId, LocalDateTime now) {
+	@Transactional
+	public void deletePurchaseTickets(DeletePurchaseTicketsCommandDto deletePurchaseTicketsCommandDto) {
+		var purchase = this.purchaseRepository.findById(deletePurchaseTicketsCommandDto.getPurchaseId()).orElseThrow(
+			PurchaseNotFoundException::new);
+		var ticketsUnderPurchase = findTicketsUnderPurchase(purchase);
+		var ticketsToRefund = this.ticketRepository.findAllById(deletePurchaseTicketsCommandDto.getTicketIds());
+		var ticketing = ticketsUnderPurchase.getFirst().getTicketing();
+
+		validatePurchaseOwnership(purchase, deletePurchaseTicketsCommandDto.getMemberEmail());
+		validateTicketingSalePeriod(ticketing, deletePurchaseTicketsCommandDto.getCommandCreatedAt());
+
+		var ticketIdUnderPurchase = ticketsUnderPurchase.stream().map(Ticket::getId).toList();
+		AtomicInteger numOfDeletedTicket = new AtomicInteger();
+		ticketsToRefund.forEach(ticket -> {
+			if (ticketIdUnderPurchase.contains(ticket.getId())) {
+				ticket.setPurchase(null);
+				numOfDeletedTicket.getAndIncrement();
+			}
+		});
+		if (numOfDeletedTicket.get() == ticketsUnderPurchase.size()) {
+			this.purchaseRepository.delete(purchase);
+		}
+	}
+
+	private void validateTicketingSalePeriod(UUID ticketingId, LocalDateTime now) {
 		var ticketing = this.ticketingRepository.findById(ticketingId).orElseThrow(
 			TicketingNotFoundException::new);
 		var saleStart = ticketing.getSaleStart();
@@ -73,5 +105,27 @@ public class PurchaseService {
 		if (now.isBefore(saleStart) || now.isAfter(saleEnd)) {
 			throw new PurchaseNotInSalePeriodException();
 		}
+	}
+
+	private void validateTicketingSalePeriod(Ticketing ticketing, LocalDateTime now) {
+		var saleStart = ticketing.getSaleStart();
+		var saleEnd = ticketing.getSaleEnd();
+		if (now.isBefore(saleStart) || now.isAfter(saleEnd)) {
+			throw new PurchaseNotInSalePeriodException();
+		}
+	}
+
+	private void validatePurchaseOwnership(Purchase purchase, String email) {
+		if (!purchase.getMember().getEmail().equals(email)) {
+			throw new AccessForNotOwnedPurchaseException();
+		}
+	}
+
+	private List<Ticket> findTicketsUnderPurchase(Purchase purchase) {
+		var ticketsUnderPurchase = this.ticketRepository.findAllByPurchase(purchase);
+		if (ticketsUnderPurchase.isEmpty()) {
+			throw new EmptyPurchaseException();
+		}
+		return ticketsUnderPurchase;
 	}
 }
--- src/test/java/com/tiketeer/Tiketeer/domain/purchase/service/PurchaseServiceTest.java
+++ src/test/java/com/tiketeer/Tiketeer/domain/purchase/service/PurchaseServiceTest.java
@@ -2,6 +2,8 @@ package com.tiketeer.Tiketeer.domain.purchase.service;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 import org.assertj.core.api.Assertions;
@@ -12,14 +14,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Limit;
+import org.springframework.data.util.Pair;
 
 import com.tiketeer.Tiketeer.domain.member.Member;
 import com.tiketeer.Tiketeer.domain.member.exception.MemberNotFoundException;
 import com.tiketeer.Tiketeer.domain.member.repository.MemberRepository;
+import com.tiketeer.Tiketeer.domain.purchase.Purchase;
+import com.tiketeer.Tiketeer.domain.purchase.exception.AccessForNotOwnedPurchaseException;
+import com.tiketeer.Tiketeer.domain.purchase.exception.EmptyPurchaseException;
 import com.tiketeer.Tiketeer.domain.purchase.exception.NotEnoughTicketException;
+import com.tiketeer.Tiketeer.domain.purchase.exception.PurchaseNotFoundException;
 import com.tiketeer.Tiketeer.domain.purchase.exception.PurchaseNotInSalePeriodException;
 import com.tiketeer.Tiketeer.domain.purchase.repository.PurchaseRepository;
 import com.tiketeer.Tiketeer.domain.purchase.service.dto.CreatePurchaseCommandDto;
+import com.tiketeer.Tiketeer.domain.purchase.service.dto.DeletePurchaseTicketsCommandDto;
 import com.tiketeer.Tiketeer.domain.role.constant.RoleEnum;
 import com.tiketeer.Tiketeer.domain.role.repository.RoleRepository;
 import com.tiketeer.Tiketeer.domain.ticket.Ticket;
@@ -68,7 +77,7 @@ public class PurchaseServiceTest {
 		// given
 		var mockEmail = "test1@test.com";
 		var member = createMember(mockEmail, "1234");
-		var ticketing = createTicketing(member.getId(), 0, 1);
+		var ticketing = createTicketing(member, 0, 1);
 
 		var createPurchaseCommand = CreatePurchaseCommandDto.builder()
 			.memberEmail(mockEmail)
@@ -107,12 +116,12 @@ public class PurchaseServiceTest {
 	}
 
 	@Test
-	@DisplayName("티케팃 판매 기간이 아님 > 구매 생성 요청 > 실패")
+	@DisplayName("티케팅 판매 기간이 아님 > 구매 생성 요청 > 실패")
 	void createPurchaseFailNotInSalePeriod() {
 		// given
 		var mockEmail = "test1@test.com";
 		var member = createMember(mockEmail, "1234");
-		var ticketing = createTicketing(member.getId(), 1, 1);
+		var ticketing = createTicketing(member, 1, 1);
 
 		var createPurchaseCommand = CreatePurchaseCommandDto.builder()
 			.memberEmail(mockEmail)
@@ -133,7 +142,7 @@ public class PurchaseServiceTest {
 		// given
 		var mockEmail = "test1@test.com";
 		var member = createMember(mockEmail, "1234");
-		var ticketing = createTicketing(member.getId(), 0, 1);
+		var ticketing = createTicketing(member, 0, 1);
 
 		var createPurchaseCommand = CreatePurchaseCommandDto.builder()
 			.memberEmail(mockEmail)
@@ -148,6 +157,159 @@ public class PurchaseServiceTest {
 		}).isInstanceOf(NotEnoughTicketException.class);
 	}
 
+	@Test
+	@DisplayName("구매 내역 일부 환불 > 티켓 환불 요청 > 성공")
+	void deletePurchaseTicketsSuccess() {
+		// given
+		var mockEmail = "test1@test.com";
+		var member = createMember(mockEmail, "1234");
+		var ticketing = createTicketing(member, 0, 5);
+		var purchaseTicketPair = createPurchase(member, ticketing, 2);
+		var purchase = purchaseTicketPair.getFirst();
+		var tickets = purchaseTicketPair.getSecond();
+
+		List<UUID> ticketsToRefund = Collections.singletonList(tickets.getFirst().getId());
+		var deletePurchaseCommand = DeletePurchaseTicketsCommandDto.builder()
+			.memberEmail(mockEmail)
+			.purchaseId(purchase.getId())
+			.ticketIds(ticketsToRefund)
+			.build();
+
+		// when
+		purchaseService.deletePurchaseTickets(deletePurchaseCommand);
+
+		// then
+		var purchaseInDbOpt = purchaseRepository.findById(purchase.getId());
+		Assertions.assertThat(purchaseInDbOpt.isPresent()).isTrue();
+
+		var ticketsUnderPurchase = ticketRepository.findAllByPurchase(purchase);
+		Assertions.assertThat(ticketsUnderPurchase.size()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("구매 내역 전체 환불 > 티켓 환불 요청 > 성공")
+	void deletePurchaseAlllTicketsSuccess() {
+		// given
+		var mockEmail = "test1@test.com";
+		var member = createMember(mockEmail, "1234");
+		var ticketing = createTicketing(member, 0, 5);
+		var purchaseTicketPair = createPurchase(member, ticketing, 2);
+		var purchase = purchaseTicketPair.getFirst();
+		var tickets = purchaseTicketPair.getSecond();
+
+		List<UUID> ticketsToRefund = tickets.stream().map(Ticket::getId).toList();
+		var deletePurchaseCommand = DeletePurchaseTicketsCommandDto.builder()
+			.memberEmail(mockEmail)
+			.purchaseId(purchase.getId())
+			.ticketIds(ticketsToRefund)
+			.build();
+
+		// when
+		purchaseService.deletePurchaseTickets(deletePurchaseCommand);
+
+		// then
+		var purchaseInDbOpt = purchaseRepository.findById(purchase.getId());
+		Assertions.assertThat(purchaseInDbOpt.isPresent()).isFalse();
+
+		var ticketsUnderPurchase = ticketRepository.findAllByPurchase(purchase);
+		Assertions.assertThat(ticketsUnderPurchase.size()).isEqualTo(0);
+	}
+
+	@Test
+	@DisplayName("구매 내역이 존재하지 않음 > 티켓 환불 요청 > 실패")
+	void deletePurchaseTicketsFailPurchaseNotFound() {
+		// given
+		var mockEmail = "test1@test.com";
+		var member = createMember(mockEmail, "1234");
+
+		List<UUID> ticketsToRefund = Collections.singletonList(UUID.randomUUID());
+		var deletePurchaseCommand = DeletePurchaseTicketsCommandDto.builder()
+			.memberEmail(mockEmail)
+			.purchaseId(UUID.randomUUID())
+			.ticketIds(ticketsToRefund)
+			.build();
+
+		Assertions.assertThatThrownBy(() -> {
+			// when
+			purchaseService.deletePurchaseTickets(deletePurchaseCommand);
+			// then
+		}).isInstanceOf(PurchaseNotFoundException.class);
+	}
+
+	@Test
+	@DisplayName("빈 구매 내역 > 티켓 환불 요청 > 실패")
+	void deletePurchaseTicketsFailEmptyPurchase() {
+		// given
+		var mockEmail = "test1@test.com";
+		var member = createMember(mockEmail, "1234");
+		var ticketing = createTicketing(member, 0, 5);
+		var purchaseTicketPair = createPurchase(member, ticketing, 0);
+		var purchase = purchaseTicketPair.getFirst();
+
+		List<UUID> ticketsToRefund = Collections.singletonList(UUID.randomUUID());
+		var deletePurchaseCommand = DeletePurchaseTicketsCommandDto.builder()
+			.memberEmail(mockEmail)
+			.purchaseId(purchase.getId())
+			.ticketIds(ticketsToRefund)
+			.build();
+
+		Assertions.assertThatThrownBy(() -> {
+			// when
+			purchaseService.deletePurchaseTickets(deletePurchaseCommand);
+			// then
+		}).isInstanceOf(EmptyPurchaseException.class);
+	}
+
+	@Test
+	@DisplayName("구매 내역 소유자가 아님 > 티켓 환불 요청 > 실패")
+	void deletePurchaseTicketsFailNotPurchaseOwner() {
+		// given
+		var mockEmail = "test1@test.com";
+		var member = createMember(mockEmail, "1234");
+		var ticketing = createTicketing(member, 0, 5);
+		var purchaseTicketPair = createPurchase(member, ticketing, 1);
+		var purchase = purchaseTicketPair.getFirst();
+		var tickets = purchaseTicketPair.getSecond();
+
+		List<UUID> ticketsToRefund = Collections.singletonList(tickets.getFirst().getId());
+		var deletePurchaseCommand = DeletePurchaseTicketsCommandDto.builder()
+			.memberEmail("another@test.com")
+			.purchaseId(purchase.getId())
+			.ticketIds(ticketsToRefund)
+			.build();
+
+		Assertions.assertThatThrownBy(() -> {
+			// when
+			purchaseService.deletePurchaseTickets(deletePurchaseCommand);
+			// then
+		}).isInstanceOf(AccessForNotOwnedPurchaseException.class);
+	}
+
+	@Test
+	@DisplayName("티켓팅 판매 기간이 아님 > 티켓 환불 요청 > 실패")
+	void deletePurchaseTicketsFailNotTicketingSalePeriod() {
+		// given
+		var mockEmail = "test1@test.com";
+		var member = createMember(mockEmail, "1234");
+		var ticketing = createTicketing(member, -2, 1);
+		var purchaseTicketPair = createPurchase(member, ticketing, 1);
+		var purchase = purchaseTicketPair.getFirst();
+		var tickets = purchaseTicketPair.getSecond();
+
+		List<UUID> ticketsToRefund = Collections.singletonList(tickets.getFirst().getId());
+		var deletePurchaseCommand = DeletePurchaseTicketsCommandDto.builder()
+			.memberEmail(mockEmail)
+			.purchaseId(purchase.getId())
+			.ticketIds(ticketsToRefund)
+			.build();
+
+		Assertions.assertThatThrownBy(() -> {
+			// when
+			purchaseService.deletePurchaseTickets(deletePurchaseCommand);
+			// then
+		}).isInstanceOf(PurchaseNotInSalePeriodException.class);
+	}
+
 	public Member createMember(String email, String password) {
 		var role = roleRepository.findByName(RoleEnum.SELLER).orElseThrow();
 		var memberForSave = Member.builder()
@@ -156,14 +318,12 @@ public class PurchaseServiceTest {
 		return memberRepository.save(memberForSave);
 	}
 
-	private Ticketing createTicketing(UUID memberId, int saleStartAfterYears, int stock) {
-		var member = memberRepository.findById(memberId).orElseThrow();
-
+	private Ticketing createTicketing(Member member, int saleStartAfterYears, int stock) {
 		var now = LocalDateTime.now();
 		var eventTime = now.plusYears(saleStartAfterYears + 2);
 		var saleStart = now.plusYears(saleStartAfterYears);
 		var saleEnd = now.plusYears(saleStartAfterYears + 1);
-		var ticketing = Ticketing.builder()
+		var ticketing = ticketingRepository.save(Ticketing.builder()
 			.price(1000)
 			.title("test")
 			.member(member)
@@ -173,11 +333,34 @@ public class PurchaseServiceTest {
 			.saleStart(saleStart)
 			.saleEnd(saleEnd)
 			.category("concert")
-			.runningMinutes(300).build();
-		ticketingRepository.save(ticketing);
+			.runningMinutes(300).build());
 		ticketRepository.saveAll(Arrays.stream(new int[stock])
 			.mapToObj(i -> Ticket.builder().ticketing(ticketing).build())
 			.toList());
 		return ticketing;
 	}
+
+	private Pair<Purchase, List<Ticket>> createPurchase(Member member, Ticketing ticketing, int count) {
+		var purchase = this.purchaseRepository.save(Purchase.builder().member(member).build());
+
+		if (count > 0) {
+			var tickets = updateTicketPurcahse(purchase, ticketing, count);
+			return Pair.of(purchase, tickets);
+		}
+		return Pair.of(purchase, Collections.emptyList());
+	}
+
+	private List<Ticket> updateTicketPurcahse(Purchase purchase, Ticketing ticketing, int count) {
+		var tickets = this.ticketRepository.findByTicketingIdAndPurchaseIsNullOrderById(ticketing.getId(),
+			Limit.of(count));
+		if (tickets.size() < count) {
+			throw new NotEnoughTicketException();
+		}
+		tickets.forEach(ticket -> {
+			ticket.setPurchase(purchase);
+			this.ticketRepository.save(ticket);
+		});
+		return tickets;
+	}
+
 }
