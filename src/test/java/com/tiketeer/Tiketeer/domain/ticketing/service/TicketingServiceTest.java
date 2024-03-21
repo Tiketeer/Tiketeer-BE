fb03392e1459e8a8db45b57a7c1bad1d22c0d1e9
One-armed-boy <fanatic5500@gmail.com>
2024-03-21 22:19:09 +0900

    TicketingService 내 미구현 테스트코드 보강

--- src/test/java/com/tiketeer/Tiketeer/domain/ticketing/service/TicketingServiceTest.java
+++ src/test/java/com/tiketeer/Tiketeer/domain/ticketing/service/TicketingServiceTest.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.IntStream;
 
 import org.assertj.core.api.Assertions;
@@ -23,6 +24,8 @@ import com.tiketeer.Tiketeer.domain.purchase.repository.PurchaseRepository;
 import com.tiketeer.Tiketeer.domain.ticket.Ticket;
 import com.tiketeer.Tiketeer.domain.ticket.repository.TicketRepository;
 import com.tiketeer.Tiketeer.domain.ticketing.Ticketing;
+import com.tiketeer.Tiketeer.domain.ticketing.exception.SaleDurationNotValidException;
+import com.tiketeer.Tiketeer.domain.ticketing.exception.TicketingNotFoundException;
 import com.tiketeer.Tiketeer.domain.ticketing.repository.TicketingRepository;
 import com.tiketeer.Tiketeer.domain.ticketing.usecase.dto.GetTicketingCommandDto;
 import com.tiketeer.Tiketeer.testhelper.TestHelper;
@@ -97,7 +100,175 @@ public class TicketingServiceTest {
 		Assertions.assertThat(result.getStock()).isEqualTo(stock);
 		Assertions.assertThat(result.getRemainedStock()).isEqualTo(stock - purchasedStock);
 		Assertions.assertThat(result.getOwner()).isEqualTo(member.getEmail());
+	}
 
+	@Test
+	@DisplayName("존재하지 않는 티케팅 > 조회 요청 > 실패")
+	void findByIdFailBecauseNotExistTicketing() {
+		// given
+		var invalidTicketingId = UUID.randomUUID();
+
+		Assertions.assertThatThrownBy(() -> {
+			// when
+			ticketingService.findById(invalidTicketingId);
+			// then
+		}).isInstanceOf(TicketingNotFoundException.class);
+	}
+
+	@Test
+	@DisplayName("존재하는 티케팅 > 조회 요청 > 성공")
+	@Transactional
+	void findByIdSuccess() {
+		// given
+		var member = testHelper.createMember("test@test.com");
+		var now = LocalDateTime.now();
+		var ticketing = ticketingService.saveTicketing(Ticketing.builder()
+			.member(member)
+			.title("tt")
+			.price(100L)
+			.runningMinutes(100)
+			.category("카테고리")
+			.location("서울 어딘가")
+			.saleStart(now.plusYears(1))
+			.saleEnd(now.plusYears(2))
+			.eventTime(now.plusYears(3))
+			.build());
+
+		// when
+		var ticketingInDB = ticketingService.findById(ticketing.getId());
+
+		// then
+		Assertions.assertThat(ticketingInDB.getId()).isEqualTo(ticketing.getId());
+	}
+
+	@Test
+	@DisplayName("유효하지 않은 판매 시작, 종료 일 > 티케팅 생성 요청 > 실패")
+	@Transactional
+	void saveTicketingFailBecauseSaleStartAndEndNotValid() {
+		// given
+		var member = testHelper.createMember("test@test.com");
+		var now = LocalDateTime.now();
+		var saleStart = now.plusYears(2);
+		var saleEnd = now.plusYears(1);
+		var eventTime = now.plusYears(3);
+		var ticketingBeforeSave = Ticketing.builder()
+			.member(member)
+			.title("tt")
+			.price(100L)
+			.runningMinutes(100)
+			.category("카테고리")
+			.location("서울 어딘가")
+			.saleStart(saleStart)
+			.saleEnd(saleEnd)
+			.eventTime(eventTime)
+			.build();
+
+		Assertions.assertThatThrownBy(() -> {
+			// when
+			ticketingService.saveTicketing(ticketingBeforeSave);
+			// then
+		}).isInstanceOf(SaleDurationNotValidException.class);
+	}
+
+	@Test
+	@DisplayName("유효하지 않은 판매 종료, 이벤트 시작 일시 > 티케팅 생성 요청 > 실패")
+	@Transactional
+	void saveTicketingFailBecauseSaleEndAndEventTimeNotValid() {
+		// given
+		var member = testHelper.createMember("test@test.com");
+		var now = LocalDateTime.now();
+		var saleStart = now.plusYears(1);
+		var saleEnd = now.plusYears(3);
+		var eventTime = now.plusYears(2);
+		var ticketingBeforeSave = Ticketing.builder()
+			.member(member)
+			.title("tt")
+			.price(100L)
+			.runningMinutes(100)
+			.category("카테고리")
+			.location("서울 어딘가")
+			.saleStart(saleStart)
+			.saleEnd(saleEnd)
+			.eventTime(eventTime)
+			.build();
+
+		Assertions.assertThatThrownBy(() -> {
+			// when
+			ticketingService.saveTicketing(ticketingBeforeSave);
+			// then
+		}).isInstanceOf(SaleDurationNotValidException.class);
+	}
+
+	@Test
+	@DisplayName("유효한 티케팅 메타데이터 > 티케팅 생성 요청 > 성공")
+	@Transactional
+	void saveTicketingSuccess() {
+		// given
+		var member = testHelper.createMember("test@test.com");
+		var now = LocalDateTime.now();
+		var saleStart = now.plusYears(1);
+		var saleEnd = now.plusYears(2);
+		var eventTime = now.plusYears(3);
+		var ticketingBeforeSave = Ticketing.builder()
+			.member(member)
+			.title("tt")
+			.price(100L)
+			.runningMinutes(100)
+			.category("카테고리")
+			.location("서울 어딘가")
+			.saleStart(saleStart)
+			.saleEnd(saleEnd)
+			.eventTime(eventTime)
+			.build();
+
+		// when
+		var ticketing = ticketingService.saveTicketing(ticketingBeforeSave);
+
+		// then
+		var ticketingOpt = ticketingRepository.findById(ticketing.getId());
+		Assertions.assertThat(ticketingOpt.isPresent()).isTrue();
+	}
+
+	@Test
+	@DisplayName("존재하지 않는 티케팅 > 티케팅 삭제 요청 > 실패")
+	void deleteTicketingFailBecauseNotExistTicketing() {
+		// given
+		var ticketingId = UUID.randomUUID();
+
+		Assertions.assertThatThrownBy(() -> {
+			// when
+			ticketingService.deleteTicketing(ticketingId);
+			// then
+		}).isInstanceOf(TicketingNotFoundException.class);
+	}
+
+	@Test
+	@DisplayName("존재하는 티케팅 > 삭제 요청 > 성공")
+	void deleteTicketingSuccess() {
+		// given
+		var member = testHelper.createMember("test@test.com");
+		var now = LocalDateTime.now();
+		var ticketing = ticketingService.saveTicketing(Ticketing.builder()
+			.member(member)
+			.title("tt")
+			.price(100L)
+			.runningMinutes(100)
+			.category("카테고리")
+			.location("서울 어딘가")
+			.saleStart(now.plusYears(1))
+			.saleEnd(now.plusYears(2))
+			.eventTime(now.plusYears(3))
+			.build());
+
+		Assertions.assertThat(ticketingService.findById(ticketing.getId()).getId()).isNotNull();
+
+		// when
+		ticketingService.deleteTicketing(ticketing.getId());
+
+		// then
+		Assertions.assertThatThrownBy(() -> {
+			ticketingService.findById(ticketing.getId());
+		}).isInstanceOf(TicketingNotFoundException.class);
 	}
 
 	private List<Ticketing> createTicketings(Member member, int count) {
--- src/test/java/com/tiketeer/Tiketeer/domain/ticketing/usecase/UpdateTicketingUseCaseTest.java
+++ src/test/java/com/tiketeer/Tiketeer/domain/ticketing/usecase/UpdateTicketingUseCaseTest.java
@@ -96,7 +96,7 @@ public class UpdateTicketingUseCaseTest {
 			.saleStart(createCmd.getSaleStart())
 			.saleEnd(createCmd.getSaleEnd())
 			.eventTime(createCmd.getEventTime())
-			.commandCreatedAt(saleStart.plusDays(1))
+			.commandCreatedAt(now)
 			.build();
 
 		Assertions.assertThatThrownBy(() -> {
