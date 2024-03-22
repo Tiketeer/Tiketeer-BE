f8a184134e07af3b0c756ef4e1f57531ebaa10a6
punkryn <gns816715@gmail.com>
2024-03-22 16:19:34 +0900

    add repository test

---
+++ src/test/java/com/tiketeer/Tiketeer/domain/member/repository/RefreshTokenRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.tiketeer.Tiketeer.domain.member.repository;
+
+import java.time.LocalDateTime;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.tiketeer.Tiketeer.domain.member.Member;
+import com.tiketeer.Tiketeer.domain.member.RefreshToken;
+import com.tiketeer.Tiketeer.domain.role.Role;
+import com.tiketeer.Tiketeer.domain.role.constant.RoleEnum;
+import com.tiketeer.Tiketeer.domain.role.repository.RoleRepository;
+
+@DataJpaTest
+class RefreshTokenRepositoryTest {
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private RefreshTokenRepository refreshTokenRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Test
+	void findByMember() {
+		Role role = roleRepository.save(new Role(RoleEnum.BUYER));
+		Member member = memberRepository.save(Member.builder()
+			.email("test@mail.com")
+			.role(role)
+			.password("1q2w3e4r!!")
+			.point(0L)
+			.enabled(true)
+			.profileUrl(null)
+			.build());
+
+		RefreshToken saved = refreshTokenRepository.save(
+			RefreshToken.builder().member(member).expiredAt(LocalDateTime.now().plusDays(7)).build());
+
+		RefreshToken refreshToken = refreshTokenRepository.findByMember(member).orElseThrow();
+
+		Assertions.assertThat(refreshToken.getId()).isEqualTo(saved.getId());
+	}
+}
