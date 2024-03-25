9d11e5ec986ab5f5308912cc45c0099d2af58f85
punkryn <gns816715@gmail.com>
2024-03-25 10:53:54 +0900

    add dependency lombok to test

--- src/test/java/com/tiketeer/Tiketeer/testhelper/dto/TestLoginResultDto.java
+++ src/test/java/com/tiketeer/Tiketeer/testhelper/dto/TestLoginResultDto.java
@@ -2,29 +2,21 @@ package com.tiketeer.Tiketeer.testhelper.dto;
 
 import com.tiketeer.Tiketeer.domain.member.Member;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(force = true)
 public class TestLoginResultDto {
-	private String accessToken;
-	private String refreshToken;
-	private Member member;
+	private final String accessToken;
+	private final String refreshToken;
+	private final Member member;
 
-	public TestLoginResultDto() {
-	}
-
+	@Builder
 	public TestLoginResultDto(String accessToken, String refreshToken, Member member) {
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
 		this.member = member;
-	}
-
-	public String getAccessToken() {
-		return accessToken;
-	}
-
-	public String getRefreshToken() {
-		return refreshToken;
-	}
-
-	public Member getMember() {
-		return member;
 	}
 }
