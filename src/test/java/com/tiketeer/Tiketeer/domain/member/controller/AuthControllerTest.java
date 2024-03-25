b81270472289ca34aebc9bd808cc8470b4bdddae
punkryn <gns816715@gmail.com>
2024-03-25 13:22:02 +0900

    fix test

--- src/test/java/com/tiketeer/Tiketeer/domain/member/controller/AuthControllerTest.java
+++ src/test/java/com/tiketeer/Tiketeer/domain/member/controller/AuthControllerTest.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.Date;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,18 +13,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tiketeer.Tiketeer.auth.constant.JwtMetadata;
-import com.tiketeer.Tiketeer.auth.jwt.JwtPayload;
 import com.tiketeer.Tiketeer.auth.jwt.JwtService;
 import com.tiketeer.Tiketeer.domain.member.constant.CookieConfig;
 import com.tiketeer.Tiketeer.domain.member.controller.dto.LoginRequestDto;
 import com.tiketeer.Tiketeer.domain.role.constant.RoleEnum;
 import com.tiketeer.Tiketeer.testhelper.TestHelper;
+import com.tiketeer.Tiketeer.testhelper.dto.TestLoginResultDto;
 
 @Import({TestHelper.class})
 @SpringBootTest
@@ -81,7 +78,10 @@ class AuthControllerTest {
 	@Test
 	@DisplayName("authorization bearer에 refresh token 추가 > 컨트롤러에 요청 > access token 확인")
 	void refreshAccessToken() throws Exception {
-		String token = jwtService.createToken(new JwtPayload("test@test.com", RoleEnum.BUYER, new Date()));
+		TestLoginResultDto testLoginResultDto = testHelper.registerAndLoginAndReturnAccessTokenAndRefreshToken(
+			"mock@mock.com", RoleEnum.BUYER);
+
+		String token = testLoginResultDto.getRefreshToken();
 
 		mockMvc
 			.perform(
