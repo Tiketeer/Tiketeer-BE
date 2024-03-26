f33f52a1bfdd868e233ba568cbb55ea8f8e58ccf
One-armed-boy <fanatic5500@gmail.com>
2024-03-27 02:12:00 +0900

    POST /ticketings EP e2e 작성

--- src/test/java/com/tiketeer/Tiketeer/domain/ticketing/controller/TicketingControllerTest.java
+++ src/test/java/com/tiketeer/Tiketeer/domain/ticketing/controller/TicketingControllerTest.java
@@ -3,6 +3,7 @@ package com.tiketeer.Tiketeer.domain.ticketing.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,6 +25,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tiketeer.Tiketeer.auth.constant.JwtMetadata;
 import com.tiketeer.Tiketeer.domain.member.Member;
 import com.tiketeer.Tiketeer.domain.member.repository.MemberRepository;
 import com.tiketeer.Tiketeer.domain.purchase.repository.PurchaseRepository;
@@ -33,11 +35,15 @@ import com.tiketeer.Tiketeer.domain.ticket.repository.TicketRepository;
 import com.tiketeer.Tiketeer.domain.ticketing.Ticketing;
 import com.tiketeer.Tiketeer.domain.ticketing.controller.dto.GetAllTicketingsResponseDto;
 import com.tiketeer.Tiketeer.domain.ticketing.controller.dto.GetTicketingResponseDto;
+import com.tiketeer.Tiketeer.domain.ticketing.controller.dto.PostTicketingRequestDto;
+import com.tiketeer.Tiketeer.domain.ticketing.controller.dto.PostTicketingResponseDto;
 import com.tiketeer.Tiketeer.domain.ticketing.repository.TicketingRepository;
 import com.tiketeer.Tiketeer.domain.ticketing.usecase.CreateTicketingUseCase;
 import com.tiketeer.Tiketeer.domain.ticketing.usecase.dto.CreateTicketingCommandDto;
 import com.tiketeer.Tiketeer.response.ApiResponse;
 import com.tiketeer.Tiketeer.testhelper.TestHelper;
+
+import jakarta.servlet.http.Cookie;
 
 @Import({TestHelper.class})
 @SpringBootTest
@@ -131,10 +137,39 @@ public class TicketingControllerTest {
 	@DisplayName("정상 컨디션 > 티케팅 생성 요청 > 성공")
 	void postTicketingSuccess() throws Exception {
 		// given
+		var now = LocalDateTime.now();
 		var email = "test@test.com";
 		var accessToken = testHelper.registerAndLoginAndReturnAccessToken(email, RoleEnum.SELLER);
+		var req = PostTicketingRequestDto.builder()
+			.title("음악회")
+			.description("설명")
+			.location("서울 강남역 8번 출구")
+			.category("음악회")
+			.runningMinutes(100)
+			.price(10000L)
+			.stock(20)
+			.eventTime(now.plusYears(3))
+			.saleStart(now.plusYears(1))
+			.saleEnd(now.plusYears(2))
+			.build();
+
 		// when
-		// then
+		mockMvc.perform(
+				post("/api/ticketings")
+					.contextPath("/api")
+					.cookie(new Cookie(JwtMetadata.ACCESS_TOKEN, accessToken))
+					.contentType(MediaType.APPLICATION_JSON)
+					.characterEncoding(StandardCharsets.UTF_8)
+					.content(objectMapper.writeValueAsString(req))
+			).andExpect(status().is2xxSuccessful())
+			.andDo(response -> {
+				var result = testHelper.getDeserializedApiResponse(response.getResponse().getContentAsString(),
+					PostTicketingResponseDto.class).getData();
+
+				// then
+				Assertions.assertThat(result.getTicketingId()).isNotNull();
+				Assertions.assertThat(ticketingRepository.findById(result.getTicketingId()).isPresent()).isTrue();
+			});
 	}
 
 	private List<Ticketing> createTicketings(Member member, int count) {
